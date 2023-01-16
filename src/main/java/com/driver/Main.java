3e301372ea0d58e0af1022f366411e53190bc558
SRINIVASAN N R <srinivasannr99@gmail.com>
2023-01-16 12:18:29 +0530

    override2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,19 +2,20 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args){
-        B obj=new B();
+        A obj=new B();
         System.out.println(obj.meth());
     }
-  
-}
-class A{
-    String meth(){
-        return "Invoking method from class A";
-    }
+    public static class A{
+        String meth(){
+            return "Invoking method from class A";
+        }
 
-}
-class B extends A{
-    String meth(){
-        return "Method is overridden in Extendend class B";
     }
+    public static class B extends A{
+        @Override
+        String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+  
 }
